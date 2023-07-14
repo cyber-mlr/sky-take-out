9a259267bd3207d34a12369882e577ef9ea72376
LiX <cyber_mlr@163.com>
2023-07-14 21:19:55 +0800

    day04 设置店铺状态

---
+++ sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -0,0 +1,32 @@
+package com.sky.controller.admin;
+
+import com.sky.config.RedisConfiguration;
+import com.sky.result.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("adminShopController")
+@Slf4j
+@RequestMapping("/admin/shop")
+public class ShopController {
+
+    public static final String KEY = "shop_status";
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @PutMapping("/{status}")
+    public Result<String> setShopStatus(@PathVariable Integer status) {
+        log.info("店铺状态设置为{}", status == 1 ? "营业中" : "打烊中");
+        redisTemplate.opsForValue().set(KEY,status);
+        return Result.success();
+    }
+
+    @GetMapping("/status")
+    public Result<Integer> getShopStatus(){
+        Integer status = (Integer) redisTemplate.opsForValue().get(KEY);
+        log.info("查询店铺状态为{}",status == 1 ? "营业中" : "打烊中");
+        return Result.success(status);
+    }
+}
